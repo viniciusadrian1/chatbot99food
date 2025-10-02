921bbcb79761745aa3099850e629011982c594e8
Vinicius Adrian <vinicius97adrian@outlook.com>
2025-10-02 18:56:01 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -290,7 +290,7 @@ def iniciar_conversa(number):
         print("Botoes falharam, enviando texto simples...")
         send_text(
             number,
-            "👋 Olá! Bem-vindo ao Cupom Premiado!\n\n🍕 Você já tem o app da 99 Food instalado?\n\n*Responda:*\n1️⃣ - Sim, já tenho\n2️⃣ - Não, preciso instalar"
+            "👋 Olá! Bem-vindo ao Cupom Premiado!\n\nResponda apenas com números\n\n🍕 Você já tem o app da 99 Food instalado?\n\n*Responda:*\n1️⃣ - Sim, já tenho\n2️⃣ - Não, preciso instalar"
         )
     
     user_states[number] = "AGUARDANDO_TEM_APP"
